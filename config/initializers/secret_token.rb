3bad0dca174f496de64040575eb5447ad672043a
Franco Brusatti <fbrusatti@gmail.com>
2014-04-25 18:14:17 -0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdTargeting::Application.config.secret_token = 'a3326ed8f904a3194073fc5332011f61dd1736060ef4ce040ad4f0d8934a6fb52cf7612189cd6b5d72061ae18526e18b6ec116190e577d418b3fadd99a18f93b'
